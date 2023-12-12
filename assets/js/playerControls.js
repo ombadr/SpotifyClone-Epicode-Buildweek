0d8f37f3393443e8cee1be1ae785896adfd42c4b
matteofalco1986 <matteo.falco1986@gmail.com>
2023-12-12 15:46:46 +0000

    Add timer and play/stop functions to player controls

--- assets/js/playerControls.js
+++ assets/js/playerControls.js
@@ -1,5 +1,9 @@
 // This script sets basic functionalities for player controls
 
+// Global constants and variables
+let barFill = 0;
+let seconds = 0;
+
 // Selezione elementi del DOM
 const playButton = document.querySelector('#playButton');
 const pauseButton = document.querySelector("#pauseButton");
@@ -7,7 +11,24 @@ const heartEmpty = document.getElementById('heartEmpty');
 const heartFilled = document.getElementById('heartFilled');
 const fullscreenIcon = document.getElementById('fullscreenIcon');
 const exitFullscreenIcon = document.getElementById('exitFullscreenIcon');
+const progressBar = document.getElementById('progressBar');
+const songTimer = document.getElementById('songTimer');
 
+
+
+// Carica gli script al caricamento della finestra
+window.onload = (event) => {
+    event.preventDefault();
+    // Alternates between showing play and pause button
+    setToggle(playButton, pauseButton);
+    // Alternates between showing filled and empty heart for favorites
+    setToggle(heartFilled, heartEmpty);
+    // Alternates between showing fullscreen and halfscreen request
+    setToggle(fullscreenIcon, exitFullscreenIcon);
+    // Sets button for fullscreen request
+    setFullscreen();
+    setPlayStop();
+}
 
 function setFullscreen() {
     // Entra schermo intero se premuto
@@ -42,15 +63,36 @@ function setToggle(toShow, toHide) {
     })
 }
 
-// Carica gli script al caricamento della finestra
-window.onload = (event) => {
-    event.preventDefault();
-    // Alternates between showing play and pause button
-    setToggle(playButton, pauseButton);
-    // Alternates between showing filled and empty heart for favorites
-    setToggle(heartFilled, heartEmpty);
-    // Alternates between showing fullscreen and halfscreen request
-    setToggle(fullscreenIcon, exitFullscreenIcon);
-    // Sets button for fullscreen request
-    setFullscreen();
+function setPlayStop() {
+    playButton.addEventListener('click', () => {
+        const startPlay = setInterval(() => {
+            progressBar.setAttribute('Value', barFill);
+            if (barFill < 100) { barFill += 1 / 100; }
+            else {
+                clearInterval(startPlay);
+                barFill = 0;
+                progressBar.setAttribute('Value', barFill);
+                playButton.classList.remove('d-none');
+                pauseButton.classList.add('d-none');
+            }
+        }, 10)
+        
+        // Aggiorna is secondi a sinistra della barra
+        const pointInTime = setInterval(() => {
+            if (seconds < 60){
+                seconds++;
+                seconds < 10 ? songTimer.innerHTML = `0:0${seconds}` : songTimer.innerHTML = `0:${seconds}`;
+            } else {
+                seconds = 0;
+                clearInterval(pointInTime);
+            }
+        }, 1000);
+        pauseButton.addEventListener('click', () => {
+            clearInterval(startPlay);
+            clearInterval(pointInTime);
+            progressBar.setAttribute('Value', barFill);
+        });
+    })
 }
+
+

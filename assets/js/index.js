533d14d4df6aa5cb08d4855bef18dbf810c51088
Omar Badr <omarbadr938@gmail.com>
2023-12-12 16:33:50 +0100

    add liked albums on homepage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,5 @@
 
 const BASE_URL = 'https://deezerdevs-deezer.p.rapidapi.com/'
-
 const options = {
     method: 'GET',
     headers: {
@@ -13,6 +12,14 @@ const ID = '122366' // Test album ID
 const artist = '50 cent' // Test artist
 
 
+
+// 5 random albums
+
+let likedAlbums = ['122366', '7090505', '496520481', '62646932', '9410100'] // 2 random albums for liked section in homepage
+
+const likedAlbumCards = document.querySelector('#likedCards')
+
+
 // getAlbumTracks(ID)
 // getTracksFromSearch(artist)
 
@@ -20,8 +27,8 @@ async function getAlbumTracks(id) {
     try {
         const data = await fetch(BASE_URL + `album/${id}`, options)
         const response = await data.json()
-        console.log(response.tracks.data)
-        return response.tracks.data
+
+        return response
     } catch (e) {
         console.error(e);
     }
@@ -31,10 +38,48 @@ async function getTracksFromSearch(artist) {
     try {
         const data = await fetch(BASE_URL + `search?q=${artist}`, options)
         const response = await data.json()
-        console.log(response.data)
-        return response.data
+        console.log(response)
+        return response
     } catch (e) {
         console.error(e);
     }
 }
 
+async function getLikedAlbumTracks() {
+    try {
+        let albumsHTML = '';
+
+        for (let i = 0; i < likedAlbums.length; i++) {
+            const albumId = likedAlbums[i];
+            const albumTracks = await getAlbumTracks(albumId);
+            const albumHTML = createLikedAlbumHTML(albumTracks);
+            albumsHTML += albumHTML;
+            console.log('Album tracks: ', albumTracks);
+        }
+
+        displayLikedAlbums(albumsHTML);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function createLikedAlbumHTML(albumTracks) {
+    const { title, label, cover_big, artist } = albumTracks;
+    return `<div class="card">
+                  <img
+                    src="${cover_big}"
+                    class="card-img-top"
+                    alt="${title}"
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title">${artist.name}</h5>
+                    <p class="card-text">${title}</p>
+                  </div>
+                </div>`;
+}
+
+function displayLikedAlbums(albumsHTML) {
+    likedAlbumCards.innerHTML = albumsHTML;
+}
+
+getLikedAlbumTracks();

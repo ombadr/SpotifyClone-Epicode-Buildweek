4185c7eb52291011bfeef6e2ec08107f7fff67d9
Omar Badr <omarbadr938@gmail.com>
2023-12-13 21:30:56 +0100

    add gabriele's modal on tracks mobile section

--- assets/js/album.js
+++ assets/js/album.js
@@ -2,100 +2,102 @@ import { convertSecondsToMinSec, generateRandomNumber, formatTime } from "./util
 
 const BASE_URL = 'https://deezerdevs-deezer.p.rapidapi.com/'
 const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '349d0315bamshe22fa1098ac0240p133261jsnab757b4a040e',
-        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-    }
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '349d0315bamshe22fa1098ac0240p133261jsnab757b4a040e',
+    'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+  }
 }
 
 const albumTracks = document.getElementById('albumTracks')
 const heroAlbum = document.getElementById('heroAlbum')
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetchSongs()
+
+  fetchSongs()
+
 })
 
 async function fetchSongs() {
-    try {
-        const urlString = window.location.href;
-        const url = new URL(urlString);
-        let albumFromUrl = url.searchParams.get('album')
+  try {
+    const urlString = window.location.href;
+    const url = new URL(urlString);
+    let albumFromUrl = url.searchParams.get('album')
 
-        console.log(albumFromUrl)
+    console.log(albumFromUrl)
 
-        const albumResults = await getTracksFromAlbum(albumFromUrl)
-
-
-        const artist = albumResults.artist.name
-        const trackList = albumResults.tracklist
-        console.log(albumResults)
-        console.log(artist)
+    const albumResults = await getTracksFromAlbum(albumFromUrl)
 
 
-        if (albumResults) {
-
-            let tracksHTML = ''
-            let heroAlbumHTML = ''
-
-            // const trackList = await getTrackList(trackListUrl)
-            // console.log('List of tracks:', trackList)
-
-            const imageAlbum = albumResults.cover_big
-            const albumTitle = albumResults.title
-            const albumArtist = albumResults.artist.name
-            const releaseDate = albumResults.release_date
-            const artistImage = albumResults.artist.picture_small
-            const numberOfTracks = albumResults.nb_tracks
-            const albumDuration = albumResults.duration
-            const formattedDuration = formatTime(albumDuration)
-
-            const heroHTML = createHeroAlbum(imageAlbum, albumTitle, albumArtist, releaseDate, artistImage, numberOfTracks, formattedDuration)
+    const artist = albumResults.artist.name
+    const trackList = albumResults.tracklist
+    console.log(albumResults)
+    console.log(artist)
 
 
-            displayHeroAlbum(heroHTML)
+    if (albumResults) {
+
+      let tracksHTML = ''
+      let heroAlbumHTML = ''
+
+      // const trackList = await getTrackList(trackListUrl)
+      // console.log('List of tracks:', trackList)
+
+      const imageAlbum = albumResults.cover_big
+      const albumTitle = albumResults.title
+      const albumArtist = albumResults.artist.name
+      const releaseDate = albumResults.release_date
+      const artistImage = albumResults.artist.picture_small
+      const numberOfTracks = albumResults.nb_tracks
+      const albumDuration = albumResults.duration
+      const formattedDuration = formatTime(albumDuration)
+
+      const heroHTML = createHeroAlbum(imageAlbum, albumTitle, albumArtist, releaseDate, artistImage, numberOfTracks, formattedDuration)
 
 
-            console.log(releaseDate)
+      displayHeroAlbum(heroHTML)
 
 
-            console.log(albumResults)
-            for (let i = 0; i < 10; i++) { // max 10 tracks
-                // const trackHTML = createAlbumSongs(trackList)
-                console.log(albumResults.tracks.data[i].title) // title
-                console.log(albumResults.tracks.data[i].duration) // duration
-                const title = albumResults.tracks.data[i].title
-                const duration = albumResults.tracks.data[i].duration
-                const trackHTML = createAlbumSongs(artist, title, duration, i + 1)
-                tracksHTML += trackHTML
-            }
-            displayAlbumSongs(tracksHTML)
+      console.log(releaseDate)
 
-        }
 
-    } catch (e) {
-        console.error(e)
+      console.log(albumResults)
+      for (let i = 0; i < 10; i++) { // max 10 tracks
+        // const trackHTML = createAlbumSongs(trackList)
+        console.log(albumResults.tracks.data[i].title) // title
+        console.log(albumResults.tracks.data[i].duration) // duration
+        const title = albumResults.tracks.data[i].title
+        const duration = albumResults.tracks.data[i].duration
+        const trackHTML = createAlbumSongs(artist, title, duration, i + 1)
+        tracksHTML += trackHTML
+      }
+      displayAlbumSongs(tracksHTML)
+
     }
+
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 async function getTracksFromAlbum(album) {
-    try {
-        const data = await fetch(BASE_URL + `album/${album}`, options)
-        const response = await data.json()
-        return response
-    } catch (e) {
-        console.error(e)
-    }
+  try {
+    const data = await fetch(BASE_URL + `album/${album}`, options)
+    const response = await data.json()
+    return response
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 
 
 function createAlbumSongs(artist, title, duration, counter) {
 
-    const randomNumber = generateRandomNumber()
-    const convertedDuration = convertSecondsToMinSec(duration)
+  const randomNumber = generateRandomNumber()
+  const convertedDuration = convertSecondsToMinSec(duration)
 
-    return `<div class="row">
+  return `<div class="row">
                     <div class="col-lg-1">${counter}</div>
                     <div class="col-lg-5 flex-grow-1">
                       <p>
@@ -111,7 +113,7 @@ function createAlbumSongs(artist, title, duration, counter) {
 }
 
 function displayAlbumSongs(albumsHTML) {
-    albumTracks.innerHTML = albumsHTML
+  albumTracks.innerHTML = albumsHTML
 }
 
 // HERO ALBUM
@@ -120,8 +122,8 @@ function displayAlbumSongs(albumsHTML) {
 
 function createHeroAlbum(image, title, artist, releaseDate, artistImage, numberOfTracks, duration) {
 
-    // duration format 53 min , 20 sec.
-    return ` <div class="col-md-4 p-4 mx-0">
+  // duration format 53 min , 20 sec.
+  return ` <div class="col-md-4 p-4 mx-0">
                     <img
                       src="${image}"
                       class="img-fluid border-0 sfumaAlbum"
@@ -150,7 +152,7 @@ function createHeroAlbum(image, title, artist, releaseDate, artistImage, numberO
 
 
 function displayHeroAlbum(heroAlbumHTML) {
-    heroAlbum.innerHTML = heroAlbumHTML
+  heroAlbum.innerHTML = heroAlbumHTML
 }
 
 // END HERO ALBUM

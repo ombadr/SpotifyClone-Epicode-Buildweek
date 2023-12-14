9debfdb16cc051785e6240fd122b658c0231fd1d
matteofalco1986 <matteo.falco1986@gmail.com>
2023-12-14 00:09:02 +0000

    Improve player functionalities

--- assets/js/playerControls.js
+++ assets/js/playerControls.js
@@ -9,14 +9,21 @@ let duration = 0;
 // Selezione elementi del DOM
 const playButton = document.querySelector("#playButton");
 const pauseButton = document.querySelector("#pauseButton");
+const backWardButton = document.getElementById('backwardButton');
+const shuffleButton = document.getElementById('shuffleBtn');
+const repeatButton = document.getElementById('repeatBtn');
 const heartEmpty = document.getElementById("heartEmpty");
 const heartFilled = document.getElementById("heartFilled");
 const fullscreenIcon = document.getElementById("fullscreenIcon");
 const exitFullscreenIcon = document.getElementById("exitFullscreenIcon");
 const progressBar = document.getElementById("progressBar");
 const songTimer = document.getElementById("songTimer");
+const songDurationDisplay = document.getElementById('songDuration');
 const volumeContainer = document.querySelector(".volumeContainer");
 const fullBar = document.querySelector(".fullBar");
+const nowPlaying = document.querySelector('.nowPlaying');
+
+
 
 // Carica gli script al caricamento della finestra
 window.onload = (event) => {
@@ -29,103 +36,11 @@ window.onload = (event) => {
   setToggle(fullscreenIcon, exitFullscreenIcon);
   // Sets button for fullscreen request
   setFullscreen();
-  setPlayStop();
+  setVolumeBar();
+  setShuffleAndRepeat();
 };
 
-function setFullscreen() {
-  // Entra schermo intero se premuto
-  fullscreenIcon.addEventListener("click", () => {
-    document.body.requestFullscreen();
-  });
-  // Esce schermo intero se premuto
-  exitFullscreenIcon.addEventListener("click", () => {
-    document.exitFullscreen();
-  });
 
-  // Controlla se il tasto escape viene premuto mentre lo schermo è fullscreen NON FUNZIONA PER ORA
-  // document.addEventListener('keydown', (event) => {
-  //     if (event.key === "Escape") {
-  //         if (document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement) {
-  //             fullscreenIcon.classList.remove('d-none');
-  //             exitFullscreenIcon.classList.add('d-none');
-  //         }
-  //     }
-  // })
-}
-
-// Toggla la visibità tra due pulsanti tramite la classe bootstrap "d-none"
-function setToggle(toShow, toHide) {
-  toShow.addEventListener("click", () => {
-    toShow.classList.toggle("d-none");
-    toHide.classList.toggle("d-none");
-  });
-  toHide.addEventListener("click", () => {
-    toShow.classList.toggle("d-none");
-    toHide.classList.toggle("d-none");
-  });
-}
-
-function setPlayStop() {
-  playButton.addEventListener("click", () => {
-    const startPlay = setInterval(() => {
-      progressBar.setAttribute("Value", barFill);
-      if (barFill < 100) {
-        barFill += (1 * 100) / barDuration / 100;
-      } else {
-        clearInterval(startPlay);
-        barFill = 0;
-        progressBar.setAttribute("Value", barFill);
-        playButton.classList.remove("d-none");
-        pauseButton.classList.add("d-none");
-      }
-    }, 10);
-
-    // Aggiorna is secondi a sinistra della barra
-    const pointInTime = setInterval(() => {
-      if (seconds < 60) {
-        seconds++;
-        seconds < 10
-          ? (songTimer.innerHTML = `0:0${seconds}`)
-          : (songTimer.innerHTML = `0:${seconds}`);
-      } else {
-        seconds = 0;
-        songTimer.innerHTML = "0:00";
-        clearInterval(pointInTime);
-      }
-    }, 1000);
-    pauseButton.addEventListener("click", () => {
-      clearInterval(startPlay);
-      clearInterval(pointInTime);
-      progressBar.setAttribute("Value", barFill);
-    });
-  });
-}
-
-volumeContainer.addEventListener("mouseenter", (event) => {
-  event.preventDefault();
-  volumeContainer.classList.add("coloredVolumeBar");
-});
-volumeContainer.addEventListener("mouseleave", (event) => {
-  event.preventDefault();
-  volumeContainer.classList.remove("coloredVolumeBar");
-});
-
-volumeContainer.addEventListener("click", function (event) {
-  // Get the click location within the component
-  const clickX = event.clientX - volumeContainer.getBoundingClientRect().left;
-  const containerSize = volumeContainer.clientWidth;
-  // const clickY = event.clientY - volumeContainer.getBoundingClientRect().top;
-  const newX = volumeContainer.clientWidth - clickX;
-
-  // Moves the bar
-  fullBar.style.right = newX + "px";
-
-  // Calculate percentage
-  const percentageFromRight = (newX * 100) / containerSize;
-  const volumePercentage = 100 - percentageFromRight;
-  volumeContainer.style["--progress-bar-transform"] = volumePercentage;
-  volume = volumePercentage / 100;
-});
 
 const url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=rhapsody";
 const options = {
@@ -149,7 +64,13 @@ async function fetchData(url, options) {
     // Parse the response as JSON
     const data = await response.json();
     const song = data.data[0];
-    playSong(song);
+    
+    // Sets info panel in song
+    setSongInfo(song);
+
+    // Sets player to play song
+    setPlaySong(song);
+    
   } catch (error) {
     // Handle errors here
     console.error("Error:", error);
@@ -159,30 +80,183 @@ async function fetchData(url, options) {
 fetchData(url, options);
 
 // Plays and stops song when triggered
-function playSong(songData) {
+function setPlaySong(songData) {
   let song = new Audio(songData.preview);
-  let songVolume = setInterval(() => {
+  setInterval(() => {
+    // Check every 100ms what the volume is and updates it in real time
     song.volume = volume;
-  }, 100);
+  }, 250);
+
+  // Updates song current time
   let songCurrentTime = setInterval(() => {
-    // console.log(song.currentTime);
-  }, 1000);
+    displayCurrentTime(song);
+  }, 100);
+
+  // Displays overal song duration
   let songDuration = setInterval(() => {
+    displaySongDuration(song)
     duration = song.duration;
     clearInterval(songDuration);
   }, 100);
-  console.log(songDuration);
+  let checkSongLoop = setInterval(() => 
+    repeatButton.classList.contains('playerControlsHighlighted') ? (song.loop = true) : (song.loop = false)
+  , 500);
+  setInterval(() => {
+    if (song.ended) {
+      pauseButton.classList.add('d-none');
+      playButton.classList.remove('d-none');
+      barFill = 0;
+      seconds = 0;
+      // clearInterval(songCurrentTime);
+      clearInterval(songDuration);
+      songCurrentTime.innerHTML = "0:00";
+    }
+  }, 500)
 
+  // Sets function of backward button
+  setSkipBackward(song);
+
+  // Inject current time to progress bar
+
+
+  // Sets button to play song
   playButton.addEventListener("click", () => {
     song.play();
-    const checkSongStatus = setInterval(() => {
-      if (song.ended) {
-        clearInterval(checkSongStatus);
-      }
-      console.log(song.ended);
-    }, 100);
   });
+
+
+  // Sets button to pauses song
   pauseButton.addEventListener("click", () => {
     song.pause();
   });
+}
+
+function setPlayParameters() {
+  const startPlay = setInterval(() => {
+    progressBar.setAttribute("Value", barFill);
+    if (barFill < 100) {
+      barFill += (1 * 100) / barDuration / 100;
+    } else {
+      clearInterval(startPlay);
+      barFill = 0;
+      progressBar.setAttribute("Value", barFill);
+      playButton.classList.remove("d-none");
+      pauseButton.classList.add("d-none");
+    }
+  }, 10);
+  pauseButton.addEventListener("click", () => {
+    clearInterval(startPlay);
+    clearInterval(pointInTime);
+    progressBar.setAttribute("Value", barFill);
+  });
+};
+
+function displaySongDuration(song) {
+  let durationToDisplay = Math.ceil(song.duration);
+  songDurationDisplay.innerHTML = `0:${durationToDisplay}`;
+}
+
+function displayCurrentTime(song) {
+  seconds = Math.ceil(song.currentTime);
+  barFill = barFillerValue(song.currentTime, song.duration);
+  progressBar.setAttribute('Value', barFill);
+  if (seconds < 10) {
+    songTimer.innerHTML = `0:0${seconds}`
+  } else {
+    songTimer.innerHTML = `0:${seconds}`
+  }
+  if(song.ended){
+    songTimer.innerHTML = "0:00";
+    barFill = 0;
+    progressBar.setAttribute('Value', 0);
+  }
+}
+
+// Returns the value of the song in percentage
+function barFillerValue(currentTime, duration){
+  return currentTime * 100 / duration;
+}
+
+// Setta a 0 il valore della canzone quando il tasto viene premuto
+function setSkipBackward (song){
+  backWardButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    if(song.currentTime > 2)
+      song.currentTime = 0;
+  })
+}
+
+// Sets fullscreen
+function setFullscreen() {
+  // Entra schermo intero se premuto
+  fullscreenIcon.addEventListener("click", () => {
+    document.body.requestFullscreen();
+  });
+  // Esce schermo intero se premuto
+  exitFullscreenIcon.addEventListener("click", () => {
+    document.exitFullscreen();
+  });
+}
+
+// Toggla la visibità tra due pulsanti tramite la classe bootstrap "d-none"
+function setToggle(toShow, toHide) {
+  toShow.addEventListener("click", () => {
+    toShow.classList.toggle("d-none");
+    toHide.classList.toggle("d-none");
+  });
+  toHide.addEventListener("click", () => {
+    toShow.classList.toggle("d-none");
+    toHide.classList.toggle("d-none");
+  });
+}
+
+// Sets volume bar functionalities
+function setVolumeBar(){
+  volumeContainer.addEventListener("mouseenter", (event) => {
+    event.preventDefault();
+    volumeContainer.classList.add("coloredVolumeBar");
+  });
+  volumeContainer.addEventListener("mouseleave", (event) => {
+    event.preventDefault();
+    volumeContainer.classList.remove("coloredVolumeBar");
+  });
+  
+  volumeContainer.addEventListener("click", function (event) {
+    // Get the click location within the component
+    const clickX = event.clientX - volumeContainer.getBoundingClientRect().left;
+    const containerSize = volumeContainer.clientWidth;
+    // const clickY = event.clientY - volumeContainer.getBoundingClientRect().top;
+    const newX = volumeContainer.clientWidth - clickX;
+  
+    // Moves the bar
+    fullBar.style.right = newX + "px";
+  
+    // Calculate percentage
+    const percentageFromRight = (newX * 100) / containerSize;
+    const volumePercentage = 100 - percentageFromRight;
+    volumeContainer.style["--progress-bar-transform"] = volumePercentage;
+    volume = volumePercentage / 100;
+  });
+}
+
+// Sets shuffle and repeat controls
+function setShuffleAndRepeat(){
+  shuffleButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    shuffleButton.classList.toggle('playerControlsHighlighted')
+  })
+  repeatButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    repeatButton.classList.toggle('playerControlsHighlighted')
+  })
+}
+
+
+function setSongInfo (song) {
+  let artistName = song.artist.name;
+  let title = song.title;
+  let imageUrl = song.artist.picture_small;
+  nowPlaying.querySelector('img').src = imageUrl;
+  nowPlaying.querySelector('div h6').innerHTML = artistName;
+  nowPlaying.querySelector('div p').innerHTML = title;
 }
